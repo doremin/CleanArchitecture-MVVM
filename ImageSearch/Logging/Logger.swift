1d8885fc4735b866d3e6ea457d77de7a14c6821a
Domin Kim <doremin98@gmail.com>
2024-01-07 07:18:41 +0900

    feat: add network logger

--- ImageSearch/Logging/Logger.swift
+++ ImageSearch/Logging/Logger.swift
@@ -16,21 +16,51 @@ protocol Loggable {
   func log(message: String, type: OSLogType)
 }
 
-final class Logger: Loggable {
-  let subsystem: String
-  let category: String
-  let osLog: OSLog
+extension Loggable {
+  func log(message: String, type: OSLogType) {
+    os_log("%@", log: osLog, type: type, message)
+  }
+}
+
+protocol NetworkLoggable {
+  func log(request: URLRequest)
+  func log(error: Error)
+  func log(responseData data: Data?, response: URLResponse?)
+}
+
+final class NetworkLogger: Loggable, NetworkLoggable {
+  var subsystem: String
+  var category: String
+  var osLog: OSLog
   
   init(
-    subsystem: String,
-    category: String)
+    subsystem: String = Bundle.main.bundleIdentifier!,
+    category: String = "Network")
   {
     self.subsystem = subsystem
     self.category = category
     self.osLog = OSLog(subsystem: subsystem, category: category)
   }
-
-  func log(message: String, type: OSLogType) {
-    os_log("%@", log: osLog, type: type, message)
+  
+  func log(request: URLRequest) {
+    #if DEBUG
+    log(message: "request: \(request.url!)", type: .debug)
+    log(message: "headers: \(request.allHTTPHeaderFields!)", type: .debug)
+    log(message: "method: \(request.httpMethod!)", type: .debug)
+    #endif
+  }
+  
+  func log(error: Error) {
+    #if DEBUG
+    log(message: "error: \(error.localizedDescription)", type: .debug)
+    #endif
+  }
+  
+  func log(responseData data: Data?, response: URLResponse?) {
+    #if DEBUG
+    guard let data = data else { return }
+    guard let dataDict = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+    log(message: "response data: \(String(describing: dataDict))", type: .debug)
+    #endif
   }
 }
